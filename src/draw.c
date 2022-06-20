8062f334c42bf6647512ee8f09d7981c7c50345b
Lei jie Xu wu <lxu-wu@fu-r5-p5.s19.be>
2022-06-20 17:47:53 +0200

    p

--- src/draw.c
+++ src/draw.c
@@ -2,36 +2,40 @@
 
 int	draw_vertline(t_data *data, int x, int y, int color, int j)
 {
-	int i;
+	int	i;
 
 	while (j < y)
 	{
 		i = (x * data->img[0]->bt / 8) + (j * data->img[0]->s_line);
 		data->img[0]->p_img[i] = color;
 		data->img[0]->p_img[++i] = color >> 8;
-		data->img[0]->p_img[++i] = color  >> 16;
+		data->img[0]->p_img[++i] = color >> 16;
 		j++;
 	}
-	return j;
+	return (j);
 }
 
-void draw_verttext(t_data *data, int x, int y, t_img *text)
+void	draw_verttext(t_data *data, int x, int y, t_img *text)
 {
-	int i = x * data->img[0]->bt / 8 + data->img[0]->s_line * y;
-	int j = data->dda->textx * (text->bt / 8) + data->dda->texty * text->s_line;
+	int	i;
+	int	j;
+
+	i = x * data->img[0]->bt / 8 + data->img[0]->s_line * y;
+	j = data->dda->textx * (text->bt / 8) + data->dda->texty * text->s_line;
 
 	data->img[0]->p_img[i] = text->p_img[j];
 	data->img[0]->p_img[++i] = text->p_img[++j];
 	data->img[0]->p_img[++i] = text->p_img[++j];
 }
 
-void			set_texture(t_data *p)
+void	set_texture(t_data *p)
 {
 	if (p->dda->side == 1 || p->dda->side == 2 || p->dda->side == 6)
 	{
 		p->dda->wallx = p->pl->posY + p->dda->walldist * p->dda->raydir_y;
 		p->dda->wallx -= floor(p->dda->wallx);
-		p->dda->textx = (int)(p->dda->wallx * (double)p->img[p->dda->side]->width);
+		p->dda->textx = (int)(p->dda->wallx
+				* (double)p->img[p->dda->side]->width);
 		if (p->dda->raydir_x > 0)
 			p->dda->textx = p->img[p->dda->side]->width - p->dda->textx - 1;
 	}
@@ -39,23 +43,26 @@ void			set_texture(t_data *p)
 	{
 		p->dda->wallx = p->pl->posX + p->dda->walldist * p->dda->raydir_x;
 		p->dda->wallx -= floor(p->dda->wallx);
-		p->dda->textx = (int)(p->dda->wallx * (double)p->img[p->dda->side]->width);
+		p->dda->textx = (int)(p->dda->wallx
+				* (double)p->img[p->dda->side]->width);
 		if (p->dda->raydir_y < 0)
 			p->dda->textx = p->img[p->dda->side]->width - p->dda->textx - 1;
 	}
 }
 
-int				draw_texture(t_data *p, int y)
+int	draw_texture(t_data *p, int y)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	set_texture(p);
 	p->dda->step = 1.0 * p->img[p->dda->side]->height / p->dda->lineheight;
-	p->dda->textpos = (p->dda->drawstart - screenHeight / 2 + p->dda->lineheight / 2) * p->dda->step;
+	p->dda->textpos = (p->dda->drawstart - screenHeight / 2
+			+ p->dda->lineheight / 2) * p->dda->step;
 	while (y < p->dda->drawend)
 	{
-		p->dda->texty = (int)p->dda->textpos & (p->img[p->dda->side]->height - 1);
+		p->dda->texty = (int)p->dda->textpos
+			& (p->img[p->dda->side]->height - 1);
 		p->dda->textpos += p->dda->step;
 		draw_verttext(p, p->dda->screenx, y, p->img[p->dda->side]);
 		y++;
@@ -64,9 +71,9 @@ int				draw_texture(t_data *p, int y)
 	return (i);
 }
 
-void			run_draw(t_data *data)
+void	run_draw(t_data *data)
 {
-	int y;
+	int	y;
 
 	y = 0;
 	data->dda->lineheight = (int)(screenHeight / data->dda->walldist);
@@ -76,7 +83,8 @@ void			run_draw(t_data *data)
 	data->dda->drawend = data->dda->lineheight / 2 + screenHeight / 2;
 	if (data->dda->drawend > screenHeight)
 		data->dda->drawend = screenHeight - 1;
-	y += draw_vertline(data, data->dda->screenx, data->dda->drawstart, data->map->C, 0);
+	y += draw_vertline(data, data->dda->screenx,
+			data->dda->drawstart, data->map->C, 0);
 	y += draw_texture(data, y);
 	draw_vertline(data, data->dda->screenx, screenHeight, data->map->F, y);
 }
--- src/init.c
+++ src/init.c
@@ -24,41 +24,50 @@ void		set_plane(t_data *ptr, char flag)
 	}
 }
 
-void			init_texture(t_data *ptr, int flag, char *path)
+void	init_texture(t_data *ptr, int flag, char *path)
 {
-	if (!(ptr->img[flag]->p_img = mlx_xpm_file_to_image(ptr->mlx,
-	path, &ptr->img[flag]->width, &ptr->img[flag]->height)))
+	ptr->img[flag]->p_img = mlx_xpm_file_to_image(ptr->mlx,
+			path, &ptr->img[flag]->width, &ptr->img[flag]->height);
+	if (!ptr->img[flag]->p_img)
 		exit(write_error("Error\nMap load\n"));
 	ptr->img[flag]->p_img = mlx_get_data_addr(ptr->img[flag]->p_img,
-	&ptr->img[flag]->bt, &ptr->img[flag]->s_line, &ptr->img[flag]->endian);
+			&ptr->img[flag]->bt, &ptr->img[flag]->s_line,
+			&ptr->img[flag]->endian);
 }
 
-void    init_data(t_data *data)
+void	init_data(t_data *data)
 {
-    data->mlx = mlx_init();
-    data->mlx_win = mlx_new_window(data->mlx, screenWidth, screenHeight, "cub3d");
+	int	i;
 
-    data->pl = malloc(sizeof(t_player));
-    data->dda = malloc(sizeof(t_dda));
-    data->map = malloc(sizeof(t_map));
-    data->t = malloc(sizeof(t_time));
-
+	data->mlx = mlx_init();
+	data->mlx_win = mlx_new_window(data->mlx,
+			screenWidth, screenHeight, "cub3d");
+	data->pl = malloc(sizeof(t_player));
+	data->dda = malloc(sizeof(t_dda));
+	data->map = malloc(sizeof(t_map));
+	data->t = malloc(sizeof(t_time));
+	if (!data->pl || !data->dda || !data->map || !data->t)
+		return ;
 	data->t->time = 0;
 	data->t->oldtime = 0;
-
-    int i = 0;
-    if (!(data->img = malloc(sizeof(t_img *) * 8)))
-        return ;
+	i = 0;
+	data->img = malloc(sizeof(t_img *) * 8);
+	if (!data->img)
+		return ;
 	while (i <= 7)
 	{
-		if (!(data->img[i] = malloc(sizeof(t_img))))
+		data->img[i] = malloc(sizeof(t_img));
+		if (!data->img[i])
 			return ;
 		i++;
 	}
 	data->sp = malloc(sizeof(t_sprite));
+	if (!data->sp)
+		return ;
 	data->sp->buffer = malloc(sizeof(double) * (screenWidth + 1));
-
-    data->pl->totalrots = 0;
+	if (!data->sp)
+		return ;
+	data->pl->totalrots = 0;
 }
 
 void	pl_init(t_data *data)
